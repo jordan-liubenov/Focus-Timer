a245f400ae64143da4492bec282f9374cadb4f62
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-05-17 01:44:17 +0300

    implemented pause function

--- src/timer.js
+++ src/timer.js
@@ -1,37 +1,30 @@
 const countdown = document.querySelector("body > div.timerBox > p");
 
 const TIMER = { minutes: 24, seconds: 59 }; //initial time at start
-
 let timer_active = false;
 
-const sleep = (ms) => {
+//buttons ------------------------------------------------------------------------------
+const startButton = document.querySelector("body > div.buttonDiv > button");
+const pauseButton = document.querySelector("body > div.buttonDiv > button:nth-child(2)");
+//--------------------------------------------------------------------------------------
+
+const sleep = (ms) => { //basic sleep function that adds delay between operations when called with the passed milliseconds
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
-const pause_timer = () => {
-  const pauseButton = document.querySelector("body > div.buttonDiv > button:nth-child(2)");
-
-  pauseButton.addEventListener("click", () => {
-    timer_active = false;
-  });
-}
-
 export const start_timer = () => {
-  const startButton = document.querySelector("body > div.buttonDiv > button");
-  const pauseButton = document.querySelector("body > div.buttonDiv > button:nth-child(2)");
-
   countdown.textContent = `${TIMER.minutes}:${TIMER.seconds}`;
 
   startButton.addEventListener("click", async (e) => {
 
     e.target.disabled = true;
-
     pauseButton.disabled = false;
-
     timer_active = true;
 
     while (timer_active) {
-      await sleep(1000);
+      if (TIMER.minutes == 0 && TIMER.seconds == 0) { //break loop when the clock hits zero
+        timer_active = false;
+      }
 
       if (TIMER.seconds > 0) {
         TIMER.seconds--;
@@ -52,12 +45,14 @@ export const start_timer = () => {
         }
       }
       countdown.textContent = `${TIMER.minutes}:${TIMER.seconds}`;
-      if (TIMER.minutes == 0 && TIMER.seconds == 0) { //break loop when 25 minutes pass
-        timer_active = false;
-      }
+      await sleep(1000);
     }
   });
+
+  pauseButton.addEventListener("click", () => {
+    countdown.textContent = `${TIMER.minutes}:${TIMER.seconds}`;
+    timer_active = false;
+    startButton.disabled = false;
+    pauseButton.disabled = true;
+  });
 };
-function log(e) {
-  console.log(e);
-}
