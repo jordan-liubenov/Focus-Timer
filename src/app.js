89a30a7d27fbb00278c4ebf871b2db84eb19fad2
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-05-19 20:21:00 +0300

    brought back quotes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import { start_timer } from "./timer.js"
+import { fetch_quotes } from "./quoteFetcher.js";
 
 const main = () => {
+  fetch_quotes();
   start_timer();
 }
 
---
+++ src/quoteFetcher.js
@@ -0,0 +1,26 @@
+const URL = "https://type.fit/api/quotes";
+
+const message_box = document.querySelector("#msg");
+
+const get_random_element = (quoteArray) => {
+  const randomizer = Math.floor(Math.random() * quoteArray.length);
+  return randomizer;
+};
+
+export const fetch_quotes = async () => {
+  try {
+    let req = await fetch(URL);
+    let res = await req.json();
+
+    let index = get_random_element(res);
+    let current_quote = res[index];
+    if (current_quote.author == null) {
+      message_box.textContent = `${current_quote.text} ~~ by Unknown`;
+    } else {
+      message_box.textContent = `${current_quote.text} ~~ by ${current_quote.author}`;
+    }
+
+  } catch (error) {
+    throw error;
+  }
+};
